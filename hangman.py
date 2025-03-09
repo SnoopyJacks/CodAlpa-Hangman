2f66d6d1eb42b831db66ae08f4325a3a943a8f82
Justine Ziola <t.nichole.2807@gmail.com>
2025-03-09 11:13:02 -0600

    adding game function
    extra notes are for myself to learn faster

--- hangman.py
+++ hangman.py
@@ -11,5 +11,24 @@ def get_random_word():
     except requests.exceptions.RequestException:
         return random.choice(["dog", "cat", "wolf", "rat"])
     
-print(get_random_word())
+"""print(get_random_word()) to test function 1"""
+    
+"""Display letter/Word"""
+def display_word(word, guessed_letters):
+    return " ".join([letter if letter in guessed_letters else "_" for letter in word])
+
+"""word = get_random_word()
+print(display_word(word, set()))
+function 2 test"""
         
+"""game function"""
+def hangman():
+    word =get_random_word()
+    guessed_letters = set()
+    attempts = 7
+    
+    print("Welcome to Hangman!")
+    print(display_word(word, guessed_letters)) #blank word
+    
+    while attempts > 0:
+        guess = input("\nGuess a letter: ").lower()
