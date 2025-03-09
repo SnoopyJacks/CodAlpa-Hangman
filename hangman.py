28d2723bb0952d2bf9cf2cc7be7017ccab9386a7
Justine Ziola <t.nichole.2807@gmail.com>
2025-03-09 10:59:57 -0600

    imports
    Note *** to import things you add them in VScode at bottom under tab called terminal
    also print getrandomword works

--- hangman.py
+++ hangman.py
@@ -1,2 +1,15 @@
 import requests
 import random
+
+"""Get word from API"""
+def get_random_word():
+    try:
+        response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
+        response.raise_for_status()
+        word =response.json()[0]
+        return word.lower()
+    except requests.exceptions.RequestException:
+        return random.choice(["dog", "cat", "wolf", "rat"])
+    
+print(get_random_word())
+        
