f64c714fcac2db86835371df1c0cda5bff370b1f
Justine Ziola <t.nichole.2807@gmail.com>
2025-03-09 13:38:58 -0600

    The game works!

--- hangman.py
+++ hangman.py
@@ -25,10 +25,38 @@ function 2 test"""
 def hangman():
     word =get_random_word()
     guessed_letters = set()
-    attempts = 7
+    attempts = 15
     
     print("Welcome to Hangman!")
     print(display_word(word, guessed_letters)) #blank word
     
     while attempts > 0:
         guess = input("\nGuess a letter: ").lower()
+        
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter")
+            continue
+        
+        if guess in guessed_letters:
+            print("You already guessed that letter")
+            continue
+        
+        guessed_letters.add(guess)  #next guess
+        
+        if guess in word:
+            print("Correct")
+        else:
+            attempts -= 1
+            print(f"Sorry, that is incorrect. You have {attempts} attempts left")
+            
+        print(display_word(word, guessed_letters)) #updates display
+        
+        if "_" not in display_word(word, guessed_letters):
+            print("\n Congratulations. The word was", word )
+            break
+        
+    else:
+        print("\n Sorry! The word was", word)
+        
+if __name__=="__main__":
+    hangman()
